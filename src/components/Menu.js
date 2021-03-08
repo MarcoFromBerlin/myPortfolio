c6bb147268d412920d385f2c316783fa155d5471
Marco Vignotto <marcovignotto81@gmail.com>
2021-03-08 12:08:35 +0100

    first commit

---
+++ src/components/Menu.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// PAges
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+
+const Menu = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* HOME at the end */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default Menu;
