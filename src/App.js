a35e62c19dc4449df746560d1eb8b465b996756f
Marco Vignotto <marcovignotto81@gmail.com>
2021-03-09 09:38:49 +0100

    Router ok

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React, { Fragment } from "react";
+
 import "./css/main.css";
-import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 const App = () => {
-  return <Menu />;
+  return (
+    <div className="container">
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/projects" component={Projects} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <Fragment>
+      <div className="menu">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Navbar;
