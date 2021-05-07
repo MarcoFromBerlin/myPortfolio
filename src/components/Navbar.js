ecc4378f25ea7f48a14ee725f4b4b6c3d6622dd2
Marco Vignotto <marcovignotto81@gmail.com>
2021-05-07 14:14:43 +0200

    Navbar Sticky code not working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 // import { NavLink } from "react-router-dom";
 import { HashLink as NavLink } from "react-router-hash-link";
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+
+  let navbarClasses = ["navbar"];
+
+  if (scrolled) {
+    navbarClasses.push("scrolled");
+
+    console.log(navbarClasses);
+  }
+
   return (
+    // <header className={x.join(" ")}>
+    //     <div className="logo">
+    //       <img src={Logo} alt="Logo" title="Logo" />
+    //     </div>
+
+    //     <nav className="navigation">
+    //         <ul>
+    //           <li><a href="#post1">Home</a></li>
+    //           <li><a href="#post2">Home</a></li>
+    //           <li><a href="#post3">Home</a></li>
+    //           <li><a href="#post4">Home</a></li>
+    //         </ul>
+    //     </nav>
+
+    // </header>
+
     <Fragment>
-      <div className="menu row-1">
-        <nav>
+      <div className={`${navbarClasses.join(" ")}" menu row-1"`}>
+        <nav className="navigation">
           <ul>
             <li>
               <NavLink exact activeClassName="menu__current__item" to="/">
