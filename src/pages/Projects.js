a2404882bdc8f4c81ee0eb16863f2308d9a3c138
Marco Vignotto <marcovignotto81@gmail.com>
2021-05-11 09:45:17 +0200

    Project section with 3 blocks ok

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,7 +5,12 @@ const Projects = () => {
     <div id="projects" className="home__main__wrap">
       <div className="row home__c center-x-y">
         <div className="col-12 t-center home__main">
-          <h1 className="text-white">Projects</h1>
+          <h1 className="section__title">Projects</h1>
+          <div className="row project__thumb__container">
+            <div className="col-4 project__thumb">1</div>
+            <div className="col-4 project__thumb">2</div>
+            <div className="col-4 project__thumb">3</div>
+          </div>
         </div>
       </div>
     </div>
