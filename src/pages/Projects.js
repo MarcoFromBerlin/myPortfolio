e9f3d68ad1b97b4e7a16de37bed01bcd73263a14
Marco Vignotto <marcovignotto81@gmail.com>
2021-05-12 13:23:50 +0200

    Modal problems

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Slide from "@material-ui/core/Slide";
 
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -23,6 +27,17 @@ const useStyles = makeStyles((theme) => ({
     position: "relative",
     margin: theme.spacing(1),
   },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paperModal: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
   svg: {
     width: 100,
     height: 100,
@@ -36,6 +51,29 @@ const useStyles = makeStyles((theme) => ({
 
 const Projects = () => {
   const classes = useStyles();
+
+  /**
+   * @description modal handles
+   */
+
+  const [open, setOpen] = useState(false);
+
+  // const handleOpen = () => {
+  //   console.log("open");
+  //   setOpen((open) => !open);
+  // };
+  const handleOpen = () => {
+    console.log("open");
+    setOpen(true);
+  };
+
+  const handleClose = (e) => {
+    e.preventDefault();
+    console.log("close", e);
+    setOpen(false);
+    console.log("open", open);
+  };
+
   /**
    * @description obj to handle the show details thumbs
    */
@@ -59,6 +97,32 @@ const Projects = () => {
     setShowDetails(objShowDetails);
   };
 
+  const ModalProject = () => {
+    return (
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paperModal}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">
+              react-transition-group animates me.
+            </p>
+          </div>
+        </Fade>
+      </Modal>
+    );
+  };
+
   return (
     <div id="projects" className="home__main__wrap">
       <div className="row home__c center-x-y">
@@ -68,14 +132,17 @@ const Projects = () => {
             A few projects I made using different tecnologies
           </h4>
           <div className="row project__thumb__container">
+            {/* <div className="btn-no-css" onClick={() => handleOpen()}> */}
             <div
               className="col-4 project__thumb"
               // onMouseEnter={() => console.log("enter")}
               // onMouseLeave={() => console.log("leave")}
               onMouseEnter={() => handleOver({ thumb: "one", value: true })}
               onMouseLeave={() => handleOver({ thumb: "one", value: false })}
-              onClick={() => console.log("open project")}
+              // onClick={() => setOpen(true)}
+              onClick={handleOpen}
             >
+              {/* <div onClick={(e) => handleOpen(e)}> */}
               <Slide
                 direction="right"
                 in={showDetails === undefined ? false : showDetails["one"]}
@@ -88,7 +155,34 @@ const Projects = () => {
                   test
                 </Paper>
               </Slide>
+              <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+              >
+                <Fade in={open}>
+                  <div className={classes.paperModal}>
+                    <h2 id="transition-modal-title">Transition modal</h2>
+                    <p id="transition-modal-description">
+                      react-transition-group animates me.
+                    </p>
+                    <button onClick={(e) => handleClose(e)}>Close</button>
+                    <button onClick={() => setOpen(false)}>Close</button>
+                  </div>
+                </Fade>
+              </Modal>
+              {/* <ModalProject /> */}
+              {/* </div> */}
             </div>
+            {/* </div> */}
+            {/* <button onClick={(e) => handleOpen(e)}></button> */}
             <div
               className="col-4 project__thumb"
               className="col-4 project__thumb"
