1bc8174b728218e11a24a7f7c8281a042ea8f286
Marco Vignotto <marcovignotto81@gmail.com>
2021-06-04 11:10:58 +0200

    Cleaned Scroll code

--- src/App.js
+++ src/App.js
@@ -76,18 +76,22 @@ const App = () => {
     return () => window.removeEventListener("resize", updateDimensions);
   }, []);
 
+  /**
+   * @desc set anchors
+   */
   useEffect(() => {
     setAnchorHome(document.querySelector("#home"));
     setAnchorProject(document.querySelector("#projects"));
     setAnchorAbout(document.querySelector("#about"));
     setAnchorMenu(document.querySelector(".menu"));
-    // console.log("useE", arrClasses);
-    // console.log(containerRef.current);
     containerRef.current.classList.remove("matrix__start");
-    // console.log(anchor.getBoundingClientRect());
 
     //eslint-disable-next-line
   }, []);
+
+  /**
+   * @desc returns the closest div in the view to the top
+   */
 
   const checkDivsPosition = () => {
     if (
@@ -119,89 +123,19 @@ const App = () => {
     // console.log("About", anchorAbout.getBoundingClientRect().top);
   };
 
+  /**
+   * @desc listeners for mobile
+   */
   useEffect(() => {
-    var mouseIsDown = false;
-
     window.addEventListener("scroll", checkDivsPosition);
     window.addEventListener("touchstart", scrollTouchStart);
     window.addEventListener("touchend", scrollTouchEnd);
-    // window.addEventListener("mousedown", function () {
-    //   console.log("mousedown");
-    //   // mouseIsDown = true;
-    //   // setTimeout(function () {
-    //   //   if (mouseIsDown) {
-    //   //     // mouse was held down for > 2 seconds
-    //   //     console.log("mouse");
-    //   //   }
-    //   // }, 2000);
-    // });
   });
 
   /**
-   * @desc function to start/stop matrix3d
-   * for desktop
+   * @desc matrix3d JS
+   * used for desktop and mobile
    */
-
-  function scrollStop(callback, refresh = 66) {
-    // Make sure a valid callback was provided
-    if (!callback || typeof callback !== "function") return;
-
-    // Setup scrolling variable
-    let isScrolling;
-
-    // Listen for scroll events
-    window.addEventListener(
-      "scroll",
-      function (event) {
-        // console.log("start", containerRef.current);
-
-        // arrClasses = [];
-        if (containerRef.current === undefined) return;
-        // if (arrClasses.length === 1) arrClasses.splice(0, 1);
-        // if (arrClasses.length >= 2) return;
-        // containerRef.current.classList.add("matrix__start");
-        containerRef.current.style.transition = "all 1s ease 0.1s";
-        containerRef.current.style.transform = matrix3dStart;
-
-        // arrClasses.push("matrix__start ");
-        // Clear our timeout throughout the scroll
-        window.clearTimeout(isScrolling);
-
-        // Set a timeout to run after scrolling ends
-        isScrolling = setTimeout(callback, refresh);
-      },
-      false
-    );
-  }
-
-  scrollStop(() => {
-    // anchorProject.scrollIntoView();
-    containerRef.current.style.transition = "all 1s ease 0.1s";
-    containerRef.current.style.transform = matrix3dStop;
-    setTimeout(() => {
-      containerRef.current.style.transition = null;
-      containerRef.current.style.transform = null;
-    }, 100);
-
-    // arrClasses.splice(0, 1);
-    // arrClasses = [];
-    // setTimeout(() => {
-    // containerRef.current.classList.remove("matrix__start");
-    //   console.log("removed");
-    // }, 2000);
-
-    // arrClasses.push("matrix__stop ");
-    // console.log("stop", arrClasses.join(" "));
-  });
-
-  /**
-   * @desc creating matrix with js
-   * for mobile
-   */
-
-  // let x = 50;
-  // let y = 100;
-  // let z = 0;
 
   let translationMatrixStart = [
     0.34299999999999997, 0, 0.17, 0, 0, 0.46199999999999997, 0.64, 0.000144,
@@ -218,82 +152,78 @@ const App = () => {
   let matrix3dStart = matrixArrayToCssMatrix(translationMatrixStart);
   let matrix3dStop = matrixArrayToCssMatrix(translationMatrixStop);
 
-  // Set the transform
+  const addMatrix = () => {
+    containerRef.current.style.transition = "all 0.75s ease 0.1s";
+    containerRef.current.style.transform = matrix3dStart;
+  };
+
+  const removeMatrix = () => {
+    containerRef.current.style.transition = "all 0.75s ease 0.1s";
+    containerRef.current.style.transform = matrix3dStop;
+    setTimeout(() => {
+      containerRef.current.style.transition = null;
+      containerRef.current.style.transform = null;
+    }, 100);
+  };
+
+  /**
+   * @desc function to start/stop matrix3d
+   * for desktop
+   */
+
+  function scrollStop(callback, refresh = 66) {
+    if (!callback || typeof callback !== "function") return;
+
+    let isScrolling;
+
+    window.addEventListener(
+      "scroll",
+      function (event) {
+        if (containerRef.current === undefined) return;
+
+        addMatrix();
+
+        window.clearTimeout(isScrolling);
+
+        isScrolling = setTimeout(callback, refresh);
+      },
+      false
+    );
+  }
+
+  scrollStop(() => {
+    removeMatrix();
+  });
+
+  /**
+   * @desc creating matrix with js
+   * for mobile
+   */
 
   const scrollTouchStart = () => {
-    containerRef.current.style.transition = "all 1s ease 0.1s";
-    containerRef.current.style.transform = matrix3dStart;
-    // console.log("into", document.querySelector("#about"));
-    // console.log("into", anchorAbout);
-    // anchorAbout.scrollIntoView({
-    //   behavior: "smooth",
-    //   block: "nearest",
-    //   inline: "end",
-    // });
-    // .scrollIntoView(true);
-    // console.log(checkDivsPosition().anchor);
-    // console.log("start");
+    addMatrix();
   };
 
   const scrollTouchEnd = () => {
-    containerRef.current.style.transition = "all 1s ease 0.1s";
-    containerRef.current.style.transform = matrix3dStop;
-    // console.log(anchorAbout);
-    // console.log("into", document.querySelector("#about"));
-    // console.log("into", anchorAbout);
-    // anchorAbout.scrollIntoView({
-    //   behavior: "smooth",
-    //   // block: "nearest",
-    //   // inline: "end",
-    // });
-    // console.log("checkDivsPosition", checkDivsPosition());
+    removeMatrix();
 
     if (checkDivsPosition() === undefined) return console.log("undef");
 
-    // console.log("window", window);
+    /**
+     * @uses checkDivsPosition
+     * to get the div to put into the view
+     */
+
     setTimeout(() => {
-      console.log(document.querySelector("#menu"));
       window.scrollTo(0, checkDivsPosition().offsetTop);
-      document
-        .querySelector("#menu")
-        .scrollTo(0, checkDivsPosition().offsetTop);
     }, 300);
-
-    // setTimeout(() => {
-    //   document
-    //     .querySelector(".menu")
-    //     .scrollTo(0, checkDivsPosition().offsetTop);
-    // }, 500);
-
-    // document.querySelector("#about").scrollIntoView({
-    //   behavior: "smooth",
-    //   block: "start",
-    //   inline: "start",
-    // });//
-
-    // console.log(checkDivsPosition().anchor);
-
-    // console.log("end");
   };
 
-  // console.log("out", arrClasses.join(" "));
   return (
     <Router>
       <MuiThemeProvider theme={theme}>
         <Fragment>
-          <div
-            // onTouchStart={scrollTouchStart}
-            // onTouchEnd={() => console.log("onTouchEnd")}
-            // onTouchEnd={scrollTouchEnd}
-            // onTouchEndCapture={() => console.log("onTouchEndCapture")}
-            // onTouchEndCapture={scrollToDiv}
-            // onTouchMove={scrollToDiv}
-            // onTransitionEnd={scrollToDiv}
-
-            ref={containerRef}
-            className={`${arrClasses.join(" ")}container`}
-            // style={{ height: "1000px" }}
-          >
+          <div ref={containerRef} className={`container`}>
             <Navbar />
             <Home windowHeight={height} />
             <Projects windowHeight={height} />
