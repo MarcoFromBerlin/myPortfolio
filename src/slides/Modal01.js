d4636e9b4a78ff25bd7afa54a9703437cf784187
Marco Vignotto <marcovignotto81@gmail.com>
2021-06-17 15:39:57 +0200

    Creating app's flows

--- src/slides/Modal01.js
+++ src/slides/Modal01.js
@@ -17,87 +17,170 @@ import mongoDbIcon from "../images/mongoDb.png";
 import reduxIcon from "../images/redux.png";
 import sc_schema_01 from "../images/sc_schema_01.png";
 
+/**
+ * @desc schemas
+ */
+
+import schema_01 from "../images/spotify_collection_01.jpeg";
+import schema_02 from "../images/spotify_collection_02.jpeg";
+import schema_03 from "../images/spotify_collection_03.jpeg";
+import schema_04 from "../images/spotify_collection_04.jpeg";
+
+const images = [
+  { img: schema_01 },
+  { img: schema_02 },
+  { img: schema_03 },
+  { img: schema_04 },
+  { img: sc_schema_01 },
+];
+
 const Modal01 = (props) => {
   const { windowHeight } = props;
 
+  const [photoIndex, setPhotoIndex] = useState(0);
   const [isImgZoomed, setIsImgZoomed] = useState(false);
 
   return (
     <div
       className="row modal__container"
-      style={{ height: isMobile ? windowHeight - 20 : windowHeight }}
+      style={{ maxHeight: isMobile ? windowHeight - 20 : windowHeight }}
     >
-      <div className="col-12 col-lg-3">
+      <div className="col-12 col-lg-4">
         <h2>My Secondhand Book Store</h2>
       </div>
-      <div className="col-12 col-lg-3">
-        <h4>Web App to organize by category your Spotify Collection</h4>
+      <div className="col-12 col-lg-4">
+        <h4>
+          Web/Mobile(iOS + Android) App manage a Secondhand Online Book Store
+        </h4>
       </div>
-      <div className="col-12 col-lg-3">
+      <div className="col-12 col-lg-4">
         <div className="slide__icons">
           <FontAwesomeIcon icon={["fab", "react"]} size="2x" />
           <img src={reduxIcon} className="modal__png__icon" alt="Redux Logo" />
-          <FontAwesomeIcon icon={["fab", "node-js"]} size="2x" />
+          <FontAwesomeIcon icon={["fab", "wordpress-simple"]} size="2x" />
           <FontAwesomeIcon icon={["fab", "chrome"]} size="2x" />
-          <img
-            src={mongoDbIcon}
-            className="modal__svg__icon"
-            alt="React Logo"
-          />
+          <FontAwesomeIcon icon={["fab", "android"]} size="2x" />
+          <FontAwesomeIcon icon={["fab", "apple"]} size="2x" />
         </div>
       </div>
-      <div className="col-6 col-lg-3">
-        <img
-          src={sc_schema_01}
-          className="img__schema"
-          alt="React Logo"
-          onClick={() => setIsImgZoomed(true)}
-        />
-        {isImgZoomed ? (
-          <Lightbox
-            mainSrc={sc_schema_01}
-            // nextSrc={images[(photoIndex + 1) % images.length]}
-            // prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-            onCloseRequest={() => setIsImgZoomed(false)}
-            // onMovePrevRequest={() =>
-            //   this.setState({
-            //     photoIndex: (photoIndex + images.length - 1) % images.length,
-            //   })
-            // }
-            // onMoveNextRequest={() =>
-            //   this.setState({
-            //     photoIndex: (photoIndex + 1) % images.length,
-            //   })
-            // }
-          />
-        ) : null}
-        <p>Go through the slides to see how it works!</p>
-      </div>
-      <div className="col-6 col-lg-3">
+      <div className="col-12 col-lg-4">
         <h4>How does it work</h4>
         <p>
-          Spotify Library is a fullstack app, multi-user app, where every user
-          can syncronize the Spotify collection and assign to every Album 3
-          genres (i.e. Rock, Pop). The user can create/edit/delete the genres,
-          filter the albums and search through them.
+          My Secondhand Book Store is an app that uses Wordpress/Woocommerce as
+          client frontend. The user enters Wordpress and can simply manage the
+          Woocommerce Shop adding books, searching them on the Google Books Api
+          and directly adding them with the cover, quantity avaible, price and
+          book status. The stock can be managed updating/deleting the items
+          directly from the app without needing the Woocommerce Product
+          Managment. Every add/update/delete made the app will be immediatly
+          visible on the WooCommerce Shop for the customers.
         </p>
         <br />
+      </div>
+      <div className="col-6 col-lg-4">
         <h4>Demo</h4>
         <p>
-          <a target="_blank" href="https://spotifylibrary.herokuapp.com/">
-            spotifylibrary.herokuapp.com/
+          <a
+            target="_blank"
+            href="https://my-second-hand-bookstore.herokuapp.com/"
+          >
+            my-second-hand-bookstore.herokuapp.com/
           </a>
         </p>
         <br />
+      </div>
+      <div className="col-6 col-lg-4">
         <h4>Links</h4>
         <p>
           <a
             target="_blank"
-            href="https://github.com/marcovignotto/my-spotify-categories"
+            href="https://github.com/marcovignotto/mybookstore"
           >
-            github.com/marcovignotto/my-spotify-categories
+            github.com/marcovignotto/mybookstore
           </a>
         </p>
+      </div>
+      <div className="col-12 col-lg-4 apps__flow">
+        <h4>App's Flow</h4>
+        <ul>
+          <li>
+            The user logs into Wordpress and access to the app integrated into
+            it - The app is protected behind the Wordpress
+          </li>
+          <li>
+            In the Insert page the user can search for a book using
+            Title/Author(s) or ISBN code - The app searches into the Google Book
+            Api
+          </li>
+          <li>
+            The user can add the book into WooCommerce with the cover, price,
+            quantity and status with a simple click - The app uses the
+            WooCommerce Api to perform CRUD operations
+          </li>
+          <li>
+            From the Database page the user can manage the books stock, updating
+            the price/quantity/status and filtering the titles- Like the Insert
+            Page the modified items will be immediatly available for the
+            customers
+          </li>
+          <li>
+            The pagination and the search are stored in Redux, the user will not
+            wait for the loading once changes the page
+          </li>
+        </ul>
+      </div>
+      <div className="col-12 t-center-x col-lg-4">
+        <p>Go through the slides to see how it works!</p>
+      </div>
+      {images.map((x, i) => {
+        if (i === 4) return;
+        return (
+          <div className="col-3 col-lg-1">
+            <img
+              src={x.img}
+              className="img__schema"
+              alt={`${x.img}`}
+              onClick={() => {
+                setPhotoIndex(i);
+                setIsImgZoomed(true);
+              }}
+            />
+          </div>
+        );
+      })}
+
+      {isImgZoomed ? (
+        <Lightbox
+          mainSrc={images[photoIndex].img}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsImgZoomed(false)}
+          onMovePrevRequest={() => {
+            if (photoIndex === 0) {
+              setPhotoIndex(0);
+              return setIsImgZoomed(false);
+            }
+            setPhotoIndex(photoIndex + images.length - 1);
+          }}
+          onMoveNextRequest={() => {
+            if (photoIndex === images.length - 1) {
+              setPhotoIndex(0);
+              return setIsImgZoomed(false);
+            }
+            setPhotoIndex(photoIndex + 1);
+          }}
+        />
+      ) : null}
+      <div className="col-12 col-lg-4">
+        <img
+          src={sc_schema_01}
+          className="img__workflow"
+          alt="Project workflow"
+          onClick={() => {
+            setPhotoIndex(4);
+            setIsImgZoomed(true);
+          }}
+        />
       </div>
     </div>
   );
--- src/slides/Modal02.js
+++ src/slides/Modal02.js
@@ -17,26 +17,46 @@ import mongoDbIcon from "../images/mongoDb.png";
 import reduxIcon from "../images/redux.png";
 import sc_schema_01 from "../images/sc_schema_01.png";
 
+/**
+ * @desc schemas
+ */
+
+import schema_01 from "../images/spotify_collection_01.jpeg";
+import schema_02 from "../images/spotify_collection_02.jpeg";
+import schema_03 from "../images/spotify_collection_03.jpeg";
+import schema_04 from "../images/spotify_collection_04.jpeg";
+
+const images = [
+  { img: schema_01 },
+  { img: schema_02 },
+  { img: schema_03 },
+  { img: schema_04 },
+  { img: sc_schema_01 },
+];
+
 const Modal02 = (props) => {
   const { windowHeight } = props;
 
+  const [photoIndex, setPhotoIndex] = useState(0);
   const [isImgZoomed, setIsImgZoomed] = useState(false);
 
   return (
     <div
       className="row modal__container"
-      style={{ height: isMobile ? windowHeight - 20 : windowHeight }}
+      style={{ maxHeight: isMobile ? windowHeight - 20 : windowHeight }}
     >
-      <div className="col-12 col-lg-3">
+      <div className="col-12 col-lg-4">
         <h2>Let's Talk About the Weather</h2>
       </div>
-      <div className="col-12 col-lg-3">
-        <h4>Web App to organize by category your Spotify Collection</h4>
+      <div className="col-12 col-lg-4">
+        <h4>
+          Web App create a weather dashboard that shows the weather of different
+          locations
+        </h4>
       </div>
-      <div className="col-12 col-lg-3">
+      <div className="col-12 col-lg-4">
         <div className="slide__icons">
-          <FontAwesomeIcon icon={["fab", "react"]} size="2x" />
-          <img src={reduxIcon} className="modal__png__icon" alt="Redux Logo" />
+          <FontAwesomeIcon icon={["fab", "js"]} size="2x" />
           <FontAwesomeIcon icon={["fab", "node-js"]} size="2x" />
           <FontAwesomeIcon icon={["fab", "chrome"]} size="2x" />
           <img
@@ -46,58 +66,115 @@ const Modal02 = (props) => {
           />
         </div>
       </div>
-      <div className="col-6 col-lg-3">
-        <img
-          src={sc_schema_01}
-          className="img__schema"
-          alt="React Logo"
-          onClick={() => setIsImgZoomed(true)}
-        />
-        {isImgZoomed ? (
-          <Lightbox
-            mainSrc={sc_schema_01}
-            // nextSrc={images[(photoIndex + 1) % images.length]}
-            // prevSrc={images[(photoIndex + images.length - 1) % images.length]}
-            onCloseRequest={() => setIsImgZoomed(false)}
-            // onMovePrevRequest={() =>
-            //   this.setState({
-            //     photoIndex: (photoIndex + images.length - 1) % images.length,
-            //   })
-            // }
-            // onMoveNextRequest={() =>
-            //   this.setState({
-            //     photoIndex: (photoIndex + 1) % images.length,
-            //   })
-            // }
-          />
-        ) : null}
-        <p>Go through the slides to see how it works!</p>
-      </div>
-      <div className="col-6 col-lg-3">
+      <div className="col-12 col-lg-4">
         <h4>How does it work</h4>
         <p>
-          Spotify Library is a fullstack app, multi-user app, where every user
-          can syncronize the Spotify collection and assign to every Album 3
-          genres (i.e. Rock, Pop). The user can create/edit/delete the genres,
-          filter the albums and search through them.
+          LsTATW is a full stack app that uses the Open Weather Api to show the
+          weather of multiple locations in a dash board. The location can be
+          insert through an Admin Panel that requires an auth. Once login in the
+          administrator can create a location with a related Username, language
+          and unit system. Accessing the frontend the dashboard will show the
+          inserted locations, the frontend user can choose the main location,
+          the location that will be bigger in the dashboard (i.e. his/her home
+          town). The app is created to collect all the weathers of a team member
+          or a class that works remotely.
+          <br></br>
+          The app is created in pure Javascript, designed with s module pattern
         </p>
         <br />
+      </div>
+      <div className="col-6 col-lg-4">
         <h4>Demo</h4>
         <p>
-          <a target="_blank" href="https://spotifylibrary.herokuapp.com/">
-            spotifylibrary.herokuapp.com/
+          <a
+            target="_blank"
+            href="https://lets-talk-about-the-weather.herokuapp.com/"
+          >
+            lets-talk-about-the-weather.herokuapp.com
+          </a>
+        </p>
+        <p>
+          <a
+            target="_blank"
+            href="https://lets-talk-about-the-weather.herokuapp.com/admin"
+          >
+            lets-talk-about-the-weather.herokuapp.com/admin
           </a>
         </p>
         <br />
+      </div>
+      <div className="col-6 col-lg-4">
         <h4>Links</h4>
         <p>
           <a
             target="_blank"
-            href="https://github.com/marcovignotto/my-spotify-categories"
+            href="https://github.com/marcovignotto/lets-talk-about-the-weather"
           >
-            github.com/marcovignotto/my-spotify-categories
+            github.com/marcovignotto/lets-talk-about-the-weather
           </a>
         </p>
+      </div>
+      <div className="col-12 col-lg-4 apps__flow">
+        <h4>App's Flow</h4>
+        <ul>
+          <li>
+            The users log in the server (Node JS /Express) - Under the hood
+            every user has an unique User Code
+          </li>
+        </ul>
+      </div>
+      <div className="col-12 t-center-x col-lg-4">
+        <p>Go through the slides to see how it works!</p>
+      </div>
+      {images.map((x, i) => {
+        if (i === 4) return;
+        return (
+          <div className="col-3 col-lg-1">
+            <img
+              src={x.img}
+              className="img__schema"
+              alt={`${x.img}`}
+              onClick={() => {
+                setPhotoIndex(i);
+                setIsImgZoomed(true);
+              }}
+            />
+          </div>
+        );
+      })}
+
+      {isImgZoomed ? (
+        <Lightbox
+          mainSrc={images[photoIndex].img}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsImgZoomed(false)}
+          onMovePrevRequest={() => {
+            if (photoIndex === 0) {
+              setPhotoIndex(0);
+              return setIsImgZoomed(false);
+            }
+            setPhotoIndex(photoIndex + images.length - 1);
+          }}
+          onMoveNextRequest={() => {
+            if (photoIndex === images.length - 1) {
+              setPhotoIndex(0);
+              return setIsImgZoomed(false);
+            }
+            setPhotoIndex(photoIndex + 1);
+          }}
+        />
+      ) : null}
+      <div className="col-12 col-lg-4">
+        <img
+          src={sc_schema_01}
+          className="img__workflow"
+          alt="Project workflow"
+          onClick={() => {
+            setPhotoIndex(4);
+            setIsImgZoomed(true);
+          }}
+        />
       </div>
     </div>
   );
